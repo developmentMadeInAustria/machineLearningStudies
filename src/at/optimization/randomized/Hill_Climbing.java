01c7ca111f61af0e1d291ba8396aa05277fd1fa4
Dominik Gail <dominik.gail@gmx.net>
2016-09-01 21:14:56 +0200

    static hill_climbing Methode in Hill Climbing hinzugefuegt Klasse Hill_Climbing zu abstract geaendert checkLeft und checkRight Helfermethoden fuer hill_climbing hinzugefuegt

--- src/at/optimization/randomized/Hill_Climbing.java
+++ src/at/optimization/randomized/Hill_Climbing.java
@@ -1,5 +1,70 @@
 package at.optimization.randomized;
 
-public class Hill_Climbing {
+import java.util.List;
 
+public abstract class Hill_Climbing {
+
+	/* 
+	 * an easy hill climbing algorithm 
+	 * 
+	 * starts with a random element and goes step for step to left or right
+	 * 
+	 * until it reaches a maximum
+	 * 
+	 */
+	public static <T extends Comparable<T>> T hillClimbing(List<T> list){		
+		
+		int workingNumber = (int) Math.random() * (list.size() - 1); 
+		T workingObject = list.get(workingNumber);
+		
+		if(checkRight(list, workingNumber)){
+			workingObject = list.get(++workingNumber);
+			while(true){
+				if(checkRight(list, workingNumber))
+					workingObject = list.get(++workingNumber);
+				else
+					break;
+			}
+		}else if(checkLeft(list, workingNumber)){
+			workingObject = list.get(--workingNumber);
+			while(true){
+				if(checkLeft(list, workingNumber))
+					workingObject = list.get(--workingNumber);
+				else
+					break;
+			}
+		}
+		
+		return workingObject;
+	}
+	
+	//if left side has a greater value, return true
+	private static <T extends Comparable<T>> boolean checkLeft(List<T> list, int objectPosition){
+		
+		if(objectPosition - 1 < 0)
+			return false;
+		
+		if(list.get(objectPosition - 1).compareTo(list.get(objectPosition)) > 0)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	//if right side has a greater value, return true
+	private static <T extends Comparable<T>> boolean checkRight(List<T> list, int objectPosition){
+		
+		if(objectPosition + 1 >= list.size())
+			return false;
+		
+		if(list.get(objectPosition + 1).compareTo(list.get(objectPosition)) > 0)
+			return true;
+		else
+			return false;
+			
+	}
+	
+	
+	
+	
 }
