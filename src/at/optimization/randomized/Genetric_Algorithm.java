45f10f5880f9060df79f7f9f2050ce1a601d4233
Dominik Gail <dominik.gail@gmx.net>
2016-09-02 16:59:27 +0200

    Genetric_Algorithm fertiggestellt

--- src/at/optimization/randomized/Genetric_Algorithm.java
+++ src/at/optimization/randomized/Genetric_Algorithm.java
@@ -1,7 +1,29 @@
 package at.optimization.randomized;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class Genetric_Algorithm {
 
-	
-	
+	public static <T extends Crossoverable<T> & Comparable<T>> List<T> genetricAlgorithm(List<T> list, int generations){
+		
+		for(int i = 0; i < generations; i++){
+			Collections.sort(list);
+			Collections.reverse(list);
+			List<T> nextGenerationList = list.subList(0, (int) (list.size() - 1) / 2);
+			if(nextGenerationList.size() % 2 != 0){
+				nextGenerationList.remove(list.size() - 1);
+			}
+			list = nextGenerationList;
+			for(int j = 0; j < 2; j++){
+				Collections.shuffle(nextGenerationList);
+				for(int k = 0; k < nextGenerationList.size(); k++){
+					list.add(nextGenerationList.get(k).crossover(nextGenerationList.get(++k)));
+				}
+			}
+		}
+		
+		return list;
+	}
 }
