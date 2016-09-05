3dfa308210262f01419ae7deebe3b9b2f8fb2516
Dominik Gail <dominik.gail@gmx.net>
2016-09-05 21:54:42 +0200

    Starting with MIMIC algorithm + Interface Distribution

---
+++ src/at/optimization/randomized/Distribution.java
@@ -0,0 +1,7 @@
+package at.optimization.randomized;
+
+public interface Distribution<E> {
+
+	public E nextElement();
+	
+}
---
+++ src/at/optimization/randomized/MIMIC.java
@@ -0,0 +1,11 @@
+package at.optimization.randomized;
+
+import java.util.function.Function;
+
+public abstract class MIMIC {
+
+	public static <T extends Distribution<E>, E> E mimicAlgorithm(T distribution, Function<E, Double> function){
+		
+	}
+	
+}
