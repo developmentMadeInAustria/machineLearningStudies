14630f648d991509dd5875518263448c34204c1c
Dominik Gail <dominik.gail@gmx.net>
2016-09-10 22:38:23 +0200

    added Constructor, Setter and Getter methodes to Cluster class

--- src/at/clustering/Cluster.java
+++ src/at/clustering/Cluster.java
@@ -1,5 +1,31 @@
 package at.clustering;
 
+import java.util.List;
+
 public class Cluster<T> {
 
+	private List<T> points;
+	private T center;
+	
+	public Cluster(List<T> points){
+		this(points, null);
+	}
+	public Cluster(List<T> points, T center){
+		this.points = points;
+		this.center = center;
+	}
+	
+	public void setPoints(List<T> points){
+		this.points = points;
+	}
+	public List<T> getPoints(){
+		return points;
+	}
+	public void setCenter(T center){
+		this.center = center;
+	}
+	public T getCenter(){
+		return center;
+	}
+	
 }
